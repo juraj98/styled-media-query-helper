751d249847c4de601824430322db8c52d5620b76
Juraj Garaj <juraj982@gmail.com>
2019-05-22 12:00:49 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 export { default as UnstyledButton } from "./components/UnstyledButton";
 export { default as UnstyledInput } from "./components/UnstyledInput";
 export { default as UnstyledSelect } from "./components/UnstyledSelect";
 export { default as UnstyledTextarea } from "./components/UnstyledTextarea";
 export { default as UnstyledToggle } from "./components/UnstyledToggle";
+=======
+export default class Media {}
+>>>>>>> Initial commit
